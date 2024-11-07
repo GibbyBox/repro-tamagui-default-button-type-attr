bc3b21f6567da6063e84099054e38b859dfc3f6d
Gibby Steier <gibby.steier@gmail.com>
2024-11-07 16:04:12 -0500

    demonstrate issue

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,48 +1,51 @@
-import { ExternalLink } from '@tamagui/lucide-icons'
-import { Anchor, H2, Paragraph, XStack, YStack } from 'tamagui'
-import { ToastControl } from 'app/CurrentToast'
+import { useState } from 'react'
+import { Button, Form, H2, H3, Input, YStack } from 'tamagui'
 
 export default function TabOneScreen() {
+
+  const [value, setValue] = useState('')
+  const [showPassword, setShowPassword] = useState(false)
   return (
     <YStack f={1} ai="center" gap="$8" px="$10" pt="$5" bg="$background">
-      <H2>Tamagui + Expo</H2>
+      <H2>Button Form Repro</H2>
 
-      <ToastControl />
+      <Form gap="$2" onSubmit={() => alert("Form submitted event")}>
+        <H3>Broken Form</H3>
+        <Input
+          placeholder="Password"
+          secureTextEntry={!showPassword}
+          blurOnSubmit={false}
+          value={value}
+          onChangeText={setValue}
+        />
+        <Button onPress={() => setShowPassword(v => !v)}>
+          Toggle password visibility
+        </Button>
 
-      <XStack ai="center" jc="center" fw="wrap" gap="$1.5" pos="absolute" b="$8">
-        <Paragraph fos="$5">Add</Paragraph>
+        <Form.Trigger mt="$2" asChild>
+          <Button>Submit</Button>
+        </Form.Trigger>
+      </Form>
 
-        <Paragraph fos="$5" px="$2" py="$1" col="$blue10" bg="$blue5">
-          tamagui.config.ts
-        </Paragraph>
+      <Form gap="$2" onSubmit={() => alert("Form submitted event")}>
+        <H3>Fixed Form</H3>
+        <Input
+          placeholder="Password"
+          secureTextEntry={!showPassword}
+          blurOnSubmit={false}
+          value={value}
+          onChangeText={setValue}
+        />
+        {/** @ts-expect-error providing html attribute */}
+        <Button type="button" onPress={() => setShowPassword(v => !v)}>
+          Toggle password visibility
+        </Button>
 
-        <Paragraph fos="$5">to root and follow the</Paragraph>
+        <Form.Trigger mt="$2" asChild>
+          <Button>Submit</Button>
+        </Form.Trigger>
+      </Form>
 
-        <XStack
-          ai="center"
-          gap="$1.5"
-          px="$2"
-          py="$1"
-          br="$3"
-          bg="$purple5"
-          hoverStyle={{ bg: '$purple6' }}
-          pressStyle={{ bg: '$purple4' }}
-        >
-          <Anchor
-            href="https://tamagui.dev/docs/core/configuration"
-            textDecorationLine="none"
-            col="$purple10"
-            fos="$5"
-          >
-            Configuration guide
-          </Anchor>
-          <ExternalLink size="$1" col="$purple10" />
-        </XStack>
-
-        <Paragraph fos="$5" ta="center">
-          to configure your themes and tokens.
-        </Paragraph>
-      </XStack>
     </YStack>
   )
 }
